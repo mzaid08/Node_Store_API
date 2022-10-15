4e5970dce2b6de5109387d47f4ea61b6d93cc41c
mzaid08 <imzaidmohd@gmailcom>
2022-10-15 12:56:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+//async error
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+//Middleware
+app.use(express.json());
+
+//route
+app.get("/", (req, res) => {
+  res.send(`<h1>Store API</h1><a href="/api/v1/products">Product Route</a>`);
+});
+
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ controllers/products.js
@@ -0,0 +1,77 @@
+const Product = require("../models/product");
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("name price");
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
+  //featured
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  //company
+  if (company) {
+    queryObject.company = company;
+  }
+  //name
+  if (name) {
+    // queryObject.name = name;
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  console.log(queryObject);
+  //numeric-filtering based on price and rating
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+  // console.log(req.query);
+  let result = Product.find(queryObject);
+  //sorting
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  //fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  //limit and skip pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
